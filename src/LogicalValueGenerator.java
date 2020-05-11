84930a44379f7a30837eec77874807c5f4c9df69
Aniket Gupta <gupta532@purdue.edu>
2020-05-11 19:24:55 +0530

    -Added a new method to convert the postfix notation expression into a tree for evaluation finally

--- src/LogicalValueGenerator.java
+++ src/LogicalValueGenerator.java
@@ -10,14 +10,19 @@ public class LogicalValueGenerator {
         String expression = sc.nextLine();
         String cleanExpression = expression.replaceAll("\\s", "");
         cleanExpression = cleanExpression.toLowerCase();
-        Queue<Character> rpnQueue = postfixConversion(cleanExpression);
+        Queue<Object> rpnQueue = postfixConversion(cleanExpression);
+        /*while (!rpnQueue.isEmpty()) {
+            System.out.print(rpnQueue.remove());
+        }*/
+
+        Node root = convertToTree(rpnQueue);
 
     }
 
 //    P ∧ q ∧ q → R
 
-    public static Queue<Character> postfixConversion(String original) throws InvalidSymbolException {
-        Queue<Character> rpnConverted = new LinkedList<>();
+    public static Queue<Object> postfixConversion(String original) throws InvalidSymbolException {
+        Queue<Object> rpnConverted = new LinkedList<>();
         Stack<LogicalSymbol> operators = new Stack<>();
 
         for (int i = 0; i < original.length(); i++) {
@@ -51,11 +56,50 @@ public class LogicalValueGenerator {
                 }
             }
         }
-
         while (!operators.isEmpty()) {
             rpnConverted.add(operators.pop().getSymbol());
         }
-
         return rpnConverted;
     }
+
+    static class Node<T> {
+        T value;
+        Node left;
+        Node right;
+
+        public Node(T value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node convertToTree(Queue<Object> rpnExpression) throws InvalidSymbolException {
+        Stack<Object> operands = new Stack<>();
+
+        while (!rpnExpression.isEmpty()) {
+            Object currElement = rpnExpression.remove();
+
+            if (Character.isLetter((Character) currElement)) {
+                operands.push(currElement);
+            } else {
+                LogicalSymbolTypeEnum parsedType = LogicalSymbolParser.getParsedSymbol((Character) currElement);
+                LogicalSymbol currentSymbol = new LogicalSymbol(parsedType);
+
+                Node tree = new Node<>(currentSymbol);
+                if ((currentSymbol).getType() == LogicalSymbolTypeEnum.NEGATION) {
+                    tree.left = new Node<>(operands.pop());
+                } else {
+                    tree.left = new Node<>(operands.pop());
+                    tree.right = new Node<>(operands.pop());
+                }
+                operands.push(tree);
+            }
+
+        }
+
+        assert operands.size() == 1; // size should be 1 because the only element in stack should be the root node
+        return (Node) operands.pop();
+    }
+
 }
